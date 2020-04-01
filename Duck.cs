1840b81e87dadd3fc609fb452d7fbe33ac17abb7
YooSeon <62490237+YooSeon@users.noreply.github.com>
2020-04-01 23:13:45 +0900

    Create Duck.cs
    SelectScene 버튼 클릭 제어 스크립트

---
+++ Duck.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Duck : MonoBehaviour
+{
+    public GameObject Oj;
+    Animator anim;
+    public Button one;
+    public Button two;
+    public Button three;
+
+    AudioSource audio;
+    GameObject sMgr;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = Oj.GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+        sMgr = GameObject.Find("AudioMgr");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(anim.GetCurrentAnimatorStateInfo(0).fullPathHash == Animator.StringToHash("Base Layer.jump"))
+        {        
+            if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f)
+            {
+                GameObject.Destroy(sMgr);
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+    }
+
+    public void ButtonOff()
+    {
+        one.interactable = false;
+        two.interactable = false;
+        three.interactable = false;
+    }
+
+    public void Soundon()
+    {
+        audio.PlayOneShot(audio.clip);
+    }
+}
