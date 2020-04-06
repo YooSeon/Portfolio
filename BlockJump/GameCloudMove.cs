3e47e283d023476e1e7ef0e5dcd81c64a0e6f42c
YooSeon <62490237+YooSeon@users.noreply.github.com>
2020-04-06 18:21:26 +0900

    GameCloudMove.cs
    GameScene 구름의 이동 제어 스크립트

---
+++ BlockJump/GameCloudMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCloudMove : MonoBehaviour
+{
+
+    Transform tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //-20
+        tr.transform.position += new Vector3(-20.0f * Time.deltaTime, 0, 0);
+    }
+
+
+}
