46bdddde14670c91c27f01b75e501ada4c073e74
YooSeon <62490237+YooSeon@users.noreply.github.com>
2020-04-06 18:28:30 +0900

    AutoParticleDelete
    생성된 파티클을 자동으로 삭제 하는 스크립트

---
+++ Pokey Ball/AutoParticleDelete.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoParticleDelete : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        Destroy(this.gameObject, 1f);
+    }
+}
