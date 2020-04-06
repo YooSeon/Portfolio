24a09c7ad522a2c0781f3ca16ab7829fe6a4c70d
YooSeon <62490237+YooSeon@users.noreply.github.com>
2020-04-06 18:23:44 +0900

    SliderHandleImage.cs
    선택 케릭터에 따른 Slider 이미지 변경 스크립트

---
+++ BlockJump/SliderHandleImage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SliderHandleImage : MonoBehaviour
+{
+    public Sprite Sheep;
+    public Sprite Duck;
+    public Sprite Peng;
+
+    Image handleImage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        handleImage = GetComponent<Image>();
+
+        if (GameObject.Find("GameManager").GetComponent<Distroy>().Name == 1)
+        {
+            handleImage.sprite = Sheep;
+        }
+        else if (GameObject.Find("GameManager").GetComponent<Distroy>().Name == 2)
+        {
+            handleImage.sprite = Duck;
+        }
+        else if (GameObject.Find("GameManager").GetComponent<Distroy>().Name == 3)
+        {
+            handleImage.sprite = Peng;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
