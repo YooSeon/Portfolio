d0deddf26d59f3838aefbc57293ae98ef5f49c6f
YooSeon <62490237+YooSeon@users.noreply.github.com>
2020-04-06 18:28:52 +0900

    Coin
    플레이어의 코인 획득을 위한 스크립트

---
+++ Pokey Ball/Coin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    float rotSpeed;
+    public UICtrl UI;
+
+    Transform rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotSpeed = 1.5f;
+        rb = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.Rotate(0,0,rotSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GameManager.instance.coin++;
+            UI.viewCoin = true;
+            Destroy(this.gameObject);
+        }
+    }
+}
