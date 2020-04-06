3d3555db29ce94b6686dd8736b9dcc7f270bd5e0
YooSeon <62490237+YooSeon@users.noreply.github.com>
2020-04-06 18:30:44 +0900

    HitEffect
    클릭할 경우 Hit 이펙트와 검은 Hole이 생성되는 스크립트

---
+++ Pokey Ball/HitEffect.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public GameObject hitEffect;
+    public GameObject hitHole;
+
+    public GameObject effect;
+   
+    GameObject TotalHithole;
+
+    // Update is called once per frame
+
+    private void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+
+           
+        
+    }
+
+    public void OnEffect(float PosY , int type, Transform ray)
+    {
+
+        GameObject hole = hitHole;
+
+        if (type == 1) // 홀, 이펙트 생성
+        {
+            Instantiate(hole, new Vector3(hole.transform.position.x, PosY, hole.transform.position.z),hole.transform.rotation,ray);
+
+            Instantiate(effect, new Vector3(effect.transform.position.x, PosY, effect.transform.position.z),effect.transform.rotation);
+
+        }
+        else //이팩트만 생성
+        {
+            Instantiate(effect, new Vector3(effect.transform.position.x, PosY, effect.transform.position.z), effect.transform.rotation);
+        }
+    }
+
+
+}
